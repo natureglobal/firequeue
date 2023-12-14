e3b17b8548d780b4268d1835327afd94062d0c9a
Goto Ippei <76621063+paaaaay5@users.noreply.github.com>
2023-12-14 14:05:13 +0900

    Remove BatchLength field from Stats struct

--- stats.go
+++ stats.go
@@ -3,7 +3,6 @@ package firequeue
 // Stats contains firequeue statistics.
 type Stats struct {
 	QueueLength      int
-	BatchLength      int
 	Success          int64
 	RetryCount       int64
 	UnretryableError int64

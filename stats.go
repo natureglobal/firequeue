3b19c0e5c54fd60fa95b29cc8b03834dd1734dce
Goto Ippei <76621063+paaaaay5@users.noreply.github.com>
2023-12-14 14:08:48 +0900

    [add]  batch length to stats

--- stats.go
+++ stats.go
@@ -3,6 +3,7 @@ package firequeue
 // Stats contains firequeue statistics.
 type Stats struct {
 	QueueLength      int
+	BatchLength      int
 	Success          int64
 	RetryCount       int64
 	UnretryableError int64

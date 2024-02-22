cd8e6d2d00b41bccf2736ac1bb27bb0a777ade8a
Goto Ippei <76621063+paaaaay5@users.noreply.github.com>
2024-02-22 17:06:39 +0900

    Refactor Stats struct in stats.go

--- stats.go
+++ stats.go
@@ -1,13 +1,12 @@
 package firequeue
 
 // Stats contains firequeue statistics.
+// Stats contains firequeue statistics.
 type Stats struct {
 	QueueLength      int
 	BatchLength      int64
 	Success          int64
-	RetrySuccess     int64
 	RetryCount       int64
 	UnretryableError int64
 	QueueFullError   int64
-	GiveupError      int64
 }
